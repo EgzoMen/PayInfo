2664302cea9f2beb7e58c48a7d1138802c0d8c3a
Egor Zenuk <egzoticmen@gmail.com>
2026-03-28 22:47:28 +0300

    Create GUI-view

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-from sources import start_window
+from sources.start_window import Ui_StartWindow
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class App(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui_components = Ui_StartWindow()
+        self.ui_components.setupUi(self)
+        self.show()
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = App()
+    app.exec()
