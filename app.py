2e5bae028090c998266949a49d04c9569a16441e
Egor Zenuk <egzoticmen@gmail.com>
2026-03-28 23:47:03 +0300

    Add pymongo and write buttons functions

--- app.py
+++ app.py
@@ -9,6 +9,17 @@ class App(QMainWindow):
         self.ui_components.setupUi(self)
         self.show()
 
+        self.ui_components.add_row_btn.clicked.connect(self.add_row)
+        self.ui_components.remove_row_btn.clicked.connect(self.remove_row)
+    
+    def add_row(self):
+        row_count = self.ui_components.product_table.rowCount()
+        self.ui_components.product_table.insertRow(row_count)
+
+    def remove_row(self):
+        row_count = self.ui_components.product_table.rowCount()
+        self.ui_components.product_table.removeRow(row_count - 1)
+
 
 if __name__ == "__main__":
     app = QApplication([])
